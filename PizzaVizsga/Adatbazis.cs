112ef0c017cc8475f926d2fabcfd7989a8c97749
Pistipisti94 <pistipisti57@gmail.com>
2024-04-29 11:12:47 +0200

    konzol kesz

--- PizzaVizsga/Adatbazis.cs
+++ PizzaVizsga/Adatbazis.cs
@@ -52,12 +52,14 @@ namespace PizzaVizsga
         internal List<Futar> GetAllFutar()
         {
             List<Futar> futars = new List<Futar>();
-            _command.CommandText = "SELECT * from futar";
+            _command.CommandText = "SELECT futar.fazon,futar.fnev, futar.ftel, SUM(pizza.par * tetel.db) AS ertek FROM futar NATURAL JOIN rendeles NATURAL JOIN tetel NATURAL JOIN pizza GROUP BY futar.fnev;";
             KapcsNyit();
             using (MySqlDataReader dr = _command.ExecuteReader())
             {
+                while (dr.Read()) { 
                 Futar futar =new Futar(dr.GetInt32("fazon"),dr.GetString("fnev"),dr.GetString("ftel"),dr.GetInt32("ertek"));
                 futars.Add(futar);
+            }
             }
             return futars;
         }
--- PizzaVizsga/Program.cs
+++ PizzaVizsga/Program.cs
@@ -14,6 +14,25 @@ namespace PizzaVizsga
         {
             futars = adatbazis.GetAllFutar();
             feladat01();
+            feladat02();
+            feladat03();
+            Console.ReadLine();
+        }
+
+        private static void feladat03()
+        {
+            int sumErtek = futars.Sum(a => a.ertek);
+            Console.WriteLine("3.feladat");
+            Console.WriteLine($"\t {sumErtek}Ft");
+        }
+
+        private static void feladat02()
+        {
+            int minErtek = futars.Min(a => a.ertek);
+            Futar futo = futars.Find(a => a.ertek == minErtek);
+            Console.WriteLine("2.feladat");
+            Console.WriteLine($"\t{futo.fnev}");
+
         }
 
         private static void feladat01()
@@ -21,8 +40,10 @@ namespace PizzaVizsga
             Console.WriteLine("1.feladat");
             foreach (var item in futars)
             {
-                Console.WriteLine($"\t {item.fazon} {item.fnev} {item.ftel} {item.ertek}");
+                Console.WriteLine($"\t {item.fazon} {item.fnev} {item.ftel}\t {item.ertek}Ft");
+
             }
+            
         }
     }
 }
